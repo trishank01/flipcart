647397547cd2c8ce7ab02a1ad549a8777158d111
tk <trishank01@gmail.com>
2023-10-21 18:37:15 +0530

    created product route

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,20 @@ import express from 'express'
 const app = express()
 import dotenv from "dotenv"
 
-
 dotenv.config({ path : 'backend/config/config.env'});
+
+
+// import all routes
+import productRoutes from "./routes/products.js"
+
+app.use("/api/v1/" , productRoutes)
+
+
+
 
 
 
 
 app.listen(process.env.PORT , () => {
-    console.log(`Server started on PORT: ${process.env.PORT}`)
+    console.log(`Server started on PORT: ${process.env.PORT} IN ${process.env.NODE_ENV} mode`)
 })
---
+++ backend/routes/products.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { getProducts } from "../controllers/productControllers.js";
+const router = express.Router()
+
+router.route("/products").get(getProducts)
+
+export default router
+
+

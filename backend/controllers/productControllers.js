16e8383c26448fa5a1db2f684f1e94811c4c1736
tk <trishank01@gmail.com>
2023-10-22 18:26:23 +0530

    implemented search product

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,11 +1,18 @@
 import CatchAsyncErrors from "../middlewares/CatchAsyncErrors.js";
 import Product from "../models/product.js";
-import ErrorHandler from "../utils/errorHandler.js";
+import APIFilters from "../utils/apiFilters.js";
+import ErrorHandler from "../utils/errorHandler.js"; 
+
 
 // Create new Product => /api/v1/products
 export const getProducts = CatchAsyncErrors(async (req, res) => {
-  const products = await Product.find();
+  const apiFilters = new APIFilters(Product , req.query).search();
+  let products = await apiFilters.query;
+
+  let filteredProductsCount = products.length
+ 
   res.status(200).json({
+    filteredProductsCount,
     products,
   });
 });

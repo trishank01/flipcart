7f3b0f562e027237556817b74458524e1218c23b
tk <trishank01@gmail.com>
2023-10-26 20:56:31 +0530

    started working on frontend created Header and footer

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Footer from "./componets/layout/Footer";
+import Header from "./componets/layout/Header";
+
+function App() {
+  return (
+    <div className="App">
+     <Header/>
+     <Footer/>
+    </div>
+  );
+}
+
+export default App;

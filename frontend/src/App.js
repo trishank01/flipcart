3674bad0858b2be6219fe2a8ee6608e898bd11fc
tk <trishank01@gmail.com>
2023-10-27 23:24:17 +0530

    add home page react-router-dom configure react-toolkit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
 import "./App.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./componets/Home";
 import Footer from "./componets/layout/Footer";
 import Header from "./componets/layout/Header";
 
 function App() {
   return (
-    <div className="App">
-     <Header/>
-     <Footer/>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home/>} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
